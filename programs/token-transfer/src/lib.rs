99f9efa63ab889714802c603cb5d7f0849d516a1
harshpreetweb3 <harshpreetweb3@gmail.com>
2024-12-07 23:08:53 +0530

    first commit

---
+++ programs/token-transfer/src/lib.rs
@@ -0,0 +1,30 @@
+#![allow(clippy::result_large_err)]
+use anchor_lang::prelude::*;
+
+pub mod instructions;
+
+use instructions::*;
+
+declare_id!("EhZhy3STZpZNXDBMoAXGZxgoMoVaLux5H7hvW2AUX5vT");
+
+#[program]
+pub mod transfer_tokens {
+    use super::*;
+
+    pub fn create_token(
+        ctx: Context<CreateToken>,
+        token_title: String,
+        token_symbol: String,
+        token_uri: String,
+    ) -> Result<()> {
+        create::create_token(ctx, token_title, token_symbol, token_uri)
+    }
+
+    pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
+        mint::mint_token(ctx, amount)
+    }
+
+    pub fn transfer_tokens(ctx: Context<TransferTokens>, amount: u64) -> Result<()> {
+        transfer::transfer_tokens(ctx, amount)
+    }
+}
